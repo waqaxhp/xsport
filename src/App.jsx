3de17036aaeaa330aa56254a8d5790ff32dfd3d8
waqaxhp <waqaxhp@gmail.com>
2024-11-12 17:38:20 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar/navbar";
+import Home from "./pages/home/home";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div className="main_container_home flex h-screen items-center justify-center bg-cover bg-center">
+        <h1 className="text-4xl font-bold text-white md:text-6xl">
+          Welcome to Our Website
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
